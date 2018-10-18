14f6442e465c8bf08daab09332582d77c0f24275
Anatoliy Popov <popov@naks.ru>
2018-10-18 15:10:43 +0300

    added click handler for main page

--- js/modal.js
+++ js/modal.js
@@ -1,32 +1,32 @@
 window.onload = function () {
-        // Get the modal
-    
-    var choices = {
-        'button_ask_question': ['modalAskQuestion', 'close-modal-button'],
-        'button_send_message': ['modalSendMessage', 'close-send-message-modal']
-    }
-    var modal = document.getElementById('modalAskQuestion');
-    
-    // Get the button that opens the modal
-    var btn = document.getElementById('button_ask_question');
 
-    // Get the <span> element that closes the modal
-    var cross = document.getElementById('close-modal-button');
-
-    // When the user clicks on the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
+    buttons = {
+        "button_ask_question": ['modal_ask_question', 'close-modal-button'],
+        "button_send_message": ['modal_send_message', 'close-modal-button']
     }
 
-    // When the user clicks on <span> (x), close the modal
-    cross.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
+        if (event.target.id !="undefined" && event.target.id in buttons) {
+            var modalshadow = document.getElementById(buttons[event.target.id][0]);
+            var crossbutton = document.getElementById(buttons[event.target.id][1]);
+            
+            modalshadow.style.display = "block";
+
+            crossbutton.onclick = function() {
+                modalshadow.style.display = "none";
+            }
+            modalshadow.addEventListener('click', function(e){
+                if (modalshadow != e.target) {return};
+                modalshadow.style.display = "none";
+            });
         }
     }
+    // how to iterate through all elements of page that have an id
+    // var elements = document.getElementsByTagName("*");
+    // var len = elements.length;
+    // for (var i=0; i<len; i++) {
+    //     if (elements[i].id != "undefined") {
+    //         console.log("ID is here: " + elements[i].id);
+    //     } 
+    // }
 }
